926e7b6917c7785eab2cc3210ec366051e80fa17
Rodrigo Arnaldo de Araújo <rodrigo.a.araujo@hotmail.com>
2019-06-09 23:23:04 -0300

    Contar pares em números crescentes e decrescentes
    Função que permite contagem em ordem crescente e em ordem decrescente dos números pares, independente da ordem que os valores são passados no parâmetro.

--- Módulo 01/02.js
+++ Módulo 01/02.js
@@ -1,9 +1,19 @@
-function pares(x, y) {
-  for (var i = x; i <= y; i++) {
-    if (i % 2 === 0) {
-      console.log(i);
-    }
-  }
-}
+        function intervaloPares(x, y) {
 
-pares(32, 321);
+            if (x >= y) {
+                for (x; y <= x; x--) {
+                    if(x%2 === 0){
+                     console.log(x);
+                    }
+                }
+            }else{
+                for (x; x <= y; x++) {
+                    if(x%2 === 0){
+                     console.log(x);
+                    }
+                }
+            } 
+        }
+
+        intervaloPares(32, 321);
+
